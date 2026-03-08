0a382900f598bb1a1630f102f5dffe65a00eadfa
Gouri Jat <gourijat26@gmail.com>
2026-03-08 13:32:36 +0530

    Updating server file

--- Backend/server.js
+++ Backend/server.js
@@ -9,9 +9,9 @@ dotenv.config()
 const app = express()
 
 connectDB()
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+
 
 app.use("/api/auth", require("./routes/authRoutes"))
 app.use("/api/contact", require("./routes/contactRoutes"))
